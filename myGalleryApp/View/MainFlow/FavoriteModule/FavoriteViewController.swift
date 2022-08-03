c5a72794f05d8ebf40ab7b139d8c02de93b28aea
Alexey Krzywicki <lexone01@gmail.com>
2022-08-03 17:58:52 +0300

    Navi title bug fix

--- myGalleryApp/View/MainFlow/FavoriteModule/FavoriteViewController.swift
+++ myGalleryApp/View/MainFlow/FavoriteModule/FavoriteViewController.swift
@@ -11,12 +11,15 @@ class FavoriteViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        title = "Избранное"
         navigationItem.rightBarButtonItem = UIBarButtonItem(image: UIImage(named: "item-search"), style: .plain, target: self, action: #selector(searchButtonTapped))
     }
     
     @objc func searchButtonTapped() {
         navigationController?.pushViewController(SearchViewController(), animated: true)
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        title = "Избранное"
     }
 
 }
--- myGalleryApp/View/MainFlow/ProfileModule/ProfileViewController.swift
+++ myGalleryApp/View/MainFlow/ProfileModule/ProfileViewController.swift
@@ -11,12 +11,15 @@ class ProfileViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        title = "Профиль"
         navigationItem.rightBarButtonItem = UIBarButtonItem(image: UIImage(named: "item-search"), style: .plain, target: self, action: #selector(searchButtonTapped))
     }
     
     @objc func searchButtonTapped() {
         navigationController?.pushViewController(SearchViewController(), animated: true)
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        title = "Профиль"
     }
 
 }
