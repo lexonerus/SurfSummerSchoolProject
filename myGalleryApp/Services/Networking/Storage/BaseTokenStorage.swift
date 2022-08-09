bde7b3cde3750b16c3148a461b53fad50bb61046
Alexey Krzywicki <lexone01@gmail.com>
2022-08-09 10:37:05 +0300

    Remove EmptyStateModule

--- myGalleryApp/Services/Networking/Storage/BaseTokenStorage.swift
+++ myGalleryApp/Services/Networking/Storage/BaseTokenStorage.swift
@@ -10,8 +10,8 @@ import Foundation
 struct BaseTokenStorage: TokenStorage {
     func getToken() throws -> TokenContainer {
         // Вечный токен, чтобы обойти авторизацию
-        TokenContainer(token: "595d9f58b8ac34689b1326e2cf4ef803882995c267a00ce34c6220f4a6d8ed6a")
-        //TokenContainer(token: "")
+        //TokenContainer(token: "595d9f58b8ac34689b1326e2cf4ef803882995c267a00ce34c6220f4a6d8ed6a")
+        TokenContainer(token: "")
     }
     
     func set(newToken: TokenContainer) throws { }
